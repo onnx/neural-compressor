228f92f63dc13e4c0d96bc2607fa97e86cc2e41b
Sun, Xuehao <xuehao.sun@intel.com>
2024-06-04 15:06:35 +0800

    test models
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/models/run_onnxrt_models_trigger.sh
+++ .azure-pipelines/scripts/models/run_onnxrt_models_trigger.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 set -eo pipefail
-# get parameters
+set -xe
 PATTERN='[-a-zA-Z0-9_]*='
 
 for i in "$@"; do
     case $i in
+    --stage=*)
+        stage=$(echo $i | sed "s/${PATTERN}//")
+        ;;
     --model=*)
         model=$(echo $i | sed "s/${PATTERN}//")
-        ;;
-    --mode=*)
-        mode=$(echo $i | sed "s/${PATTERN}//")
-        ;;
-    --USE_TUNE_ACC=*)
-        USE_TUNE_ACC=$(echo $i | sed "s/${PATTERN}//")
-        ;;
-    --PERF_STABLE_CHECK=*)
-        PERF_STABLE_CHECK=$(echo $i | sed "s/${PATTERN}//")
-        ;;
-    --BUILD_BUILDID=*)
-        BUILD_BUILDID=$(echo $i | sed "s/${PATTERN}//")
         ;;
     *)
         echo "Parameter $i not recognized."
@@ -27,41 +18,42 @@ for i in "$@"; do
     esac
 done
 
-echo "specify FWs version..."
+model_src_dir="/neural-compressor/examples/nlp/huggingface_model/text_generation/llama/quantization/weight_only"
+batch_size=16
+function run_prepare_model() {
+    python prepare_model.py --input_model="$model" \
+        --output_model="./model_export" \
+        --task=text-generation-with-past \ # or text-generatio
+}
 
-FRAMEWORK="onnxrt"
-FRAMEWORK_VERSION='1.17.1'
+function run_quantize() {
+    bash run_quant.sh --input_model="./model_export" \
+        --output_model="./model_tune" \
+        --batch_size="$batch_size" \
+        --dataset=NeelNanda/pile-10k \
+        --tokenizer="$model" \
+        --algorithm=WOQ_TUNE
+}
 
-inc_new_api=false
-# ======== set up config for onnxrt models ========
-if [ "${model}" == "resnet50-v1-12" ]; then
-    model_src_dir="image_recognition/onnx_model_zoo/resnet50/quantization/ptq_static"
-    dataset_location="/tf_dataset2/datasets/imagenet/ImagenetRaw/ImagenetRaw_small_5000/ILSVRC2012_img_val"
-    input_model="/tf_dataset2/models/onnx/resnet50-v1-12/resnet50-v1-12.onnx"
-    yaml="resnet50_v1_5.yaml"
-    strategy="basic"
-    batch_size=1
-    new_benchmark=true
-    inc_new_api=true
-    tuning_cmd="bash run_quant.sh --input_model=${input_model} --dataset_location=${dataset_location}"
-    benchmark_cmd="bash run_benchmark.sh --config=${yaml} --mode=performance --dataset_location=${dataset_location}"
-fi
+function run_accuray() {
+    bash run_benchmark.sh --input_model="./model_tune" \
+        --batch_size="$batch_size" \
+        --mode=accuracy \
+        --tokenizer="$model" \
+        --tasks=lambada_openai | tee -a accuracy.log
+}
 
-/bin/bash run_model_trigger_common.sh \
-    --yaml=${yaml} \
-    --framework=${FRAMEWORK} \
-    --fwk_ver=${FRAMEWORK_VERSION} \
-    --model=${model} \
-    --model_src_dir=${model_src_dir} \
-    --dataset_location=${dataset_location} \
-    --input_model=${input_model} \
-    --batch_size=${batch_size} \
-    --strategy=${strategy} \
-    --new_benchmark=${new_benchmark} \
-    --tuning_cmd="${tuning_cmd}" \
-    --benchmark_cmd="${benchmark_cmd}" \
-    --inc_new_api="${inc_new_api}" \
-    --mode=${mode} \
-    --USE_TUNE_ACC=${USE_TUNE_ACC} \
-    --PERF_STABLE_CHECK=${PERF_STABLE_CHECK} \
-    --BUILD_BUILDID=${BUILD_BUILDID}
+function main() {
+    cd $model_src_dir
+    if [ "$stage" == "prepare_model" ]; then
+        run_prepare_model
+    elif [ "$stage" == "quantize" ]; then
+        run_quantize
+    elif [ "$stage" == "accuracy" ]; then
+        run_accuray
+    else
+        exit 1
+    fi
+}
+
+main
