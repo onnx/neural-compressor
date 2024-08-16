be59ac4d3879e73afa2116e4849fddce8215bebc
Mengni Wang <mengni.wang@intel.com>
2024-08-16 02:46:07 -0700

    improve ut coverage
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

--- test/quantization/test_algorithm_utility.py
+++ test/quantization/test_algorithm_utility.py
@@ -6,6 +6,7 @@ import unittest
 import numpy as np
 import onnx
 
+from onnx_neural_compressor import onnx_model
 from onnx_neural_compressor.algorithms import utility as quant_utils
 
 
@@ -40,6 +41,40 @@ class TestUtilityFunctions(unittest.TestCase):
             beta=0.35,
         )
         self.assertFalse(quant_utils.is_B_transposed(node))
+
+    def test_make_woq_dq_node(self):
+        node = onnx.helper.make_node("MatMul", ["input", "weight"], "output", name="Matmul")
+        with self.assertRaises(ValueError):
+            quant_utils.make_weight_only_dequant_node(
+                node=node,
+                weight_shape=(32, 32),
+                block_size=16,
+                num_bits=32,
+                dtype="int",
+                q_weight=np.random.randint(0, 10, size=(2, 32), dtype=np.uint8),
+                scale=np.random.random((2, 32)),
+                zero_point=np.zeros((2, 32)),
+            )
+
+    def test_split_shared_bias(self):
+        input = onnx.helper.make_tensor_value_info("input", onnx.TensorProto.FLOAT, [1, 3, 15, 15])
+        output = onnx.helper.make_tensor_value_info("output", onnx.TensorProto.FLOAT, [1, 5, 11, 11])
+        bias_initializer = onnx.numpy_helper.from_array(np.random.random(5).astype(np.float32), name="bias")
+        conv1_weight_initializer = onnx.numpy_helper.from_array(
+            np.random.randint(-1, 2, [5, 3, 3, 3]).astype(np.float32), name="conv1_weight"
+        )
+        conv1_node = onnx.helper.make_node("Conv", ["add_out", "conv1_weight", "bias"], ["conv1_output"], name="conv1")
+        conv2_weight_initializer = onnx.numpy_helper.from_array(
+            np.random.randint(-1, 2, [5, 5, 3, 3]).astype(np.float32), name="conv2_weight"
+        )
+        conv2_node = onnx.helper.make_node("Conv", ["add_out", "conv2_weight", "bias"], ["conv2_output"], name="conv2")
+        initializers = [conv1_weight_initializer, conv2_weight_initializer, bias_initializer]
+        graph = onnx.helper.make_graph([conv1_node, conv2_node], "test", [input], [output], initializer=initializers)
+        model = onnx.helper.make_model(graph, opset_imports=[onnx.helper.make_opsetid("", 13)])
+
+        update_model = quant_utils.split_shared_bias(onnx_model.ONNXModel(model))
+        split = any(["_nc_split_" in i.name for i in update_model.initializer()])
+        self.assertTrue(split)
 
 
 if __name__ == "__main__":
